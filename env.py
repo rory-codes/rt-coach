3e8b8e6047960be1b0dc8f6edff9f71b62302f97
rory-codes <rory.thompson@outlook.com>
2025-10-18 15:29:32 +0100

    Add postgres database

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",    
+    "postgresql://neondb_owner:npg_L2mdMsWqYy0k@ep-nameless-violet-ag1wzstl.c-2.eu-central-1.aws.neon.tech/amigo_art_emu_896952",
+    )
