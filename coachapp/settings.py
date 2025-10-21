b37e3bee23b3fb9e4448ec53beeff90aae7edbfb
rory-codes <rory.thompson@outlook.com>
2025-10-21 19:45:54 +0100

    enable serving of static files

--- coachapp/settings.py
+++ coachapp/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'coachapp.urls'
@@ -146,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"     
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATICFILES_DIRS = [BASE_DIR / "static"]    
 
 # Default primary key field type
