cc17db038239a4a1eb70d97113f08e097baa672e
rory-codes <rory.thompson@outlook.com>
2025-10-22 19:03:24 +0100

    add fitness_data URLs and template for fitness data display

--- coachapp/urls.py
+++ coachapp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(("blog.urls", "blog"), namespace="blog")),
+    path("fitness_data/", include("fitness_data.urls"), name="fitness_data-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
