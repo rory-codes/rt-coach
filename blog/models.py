21d93aadd1c063bbfba7dd6600f7467be5123584
rory-codes <rory.thompson@outlook.com>
2025-10-27 08:31:47 +0000

    Blog: featured_image migration + tidy gitignore

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', blank=True, null=True)
 
     class Meta:
         ordering = ["-created_on"]
